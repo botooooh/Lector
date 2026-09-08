6967ea8f20360963a26d993f0479e35ecd79a8bc
botooooh <botcompte763@gmail.com>
2026-09-08 21:34:04 +0000

    feat: convert app to PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'Lector logo.png'],
+      manifest: {
+        name: 'Lector Music Player',
+        short_name: 'Lector',
+        description: 'A beautiful local and YouTube music player.',
+        theme_color: '#381E72',
+        background_color: '#F4EFF4',
+        icons: [
+          {
+            src: 'Lector logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
